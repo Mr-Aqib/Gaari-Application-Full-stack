e2cec1dc650a6e6463b17f0a3a877729cc319ba6
Aqib1422 <mraqibshafique@gmail.com>
2024-09-16 18:18:01 +0500

    user service slice

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
+import authRedcuer from '../features/auth/authSlice'
 export const store = configureStore({
   reducer: {
-    
+    auth:authRedcuer
   },
 });
